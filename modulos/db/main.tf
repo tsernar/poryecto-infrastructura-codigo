b16c2e211226c3012675cf1b755de285d1295326
tsernar <tsernar@unal.edu.co>
2025-12-15 01:05:13 -0500

    actualizacion archivos db

--- modulos/db/main.tf
+++ modulos/db/main.tf
@@ -1,54 +1,45 @@
+resource "aws_security_group" "db" {
+  name        = "db-sg-${var.environment}"
+  description = "DB access"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "Postgres/MySQL from VPC (simple demo)"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/8"] # simple; en real lo restringes al SG de la app
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_db_subnet_group" "this" {
   name       = "db-subnet-${var.environment}"
   subnet_ids = var.private_subnet_ids
-
-  tags = {
-    Name = "db-subnet-${var.environment}"
-  }
 }
 
-resource "aws_security_group" "db" {
-  name        = "db-sg-${var.environment}"
-  description = "Acceso a RDS desde la app"
-  vpc_id      = var.vpc_id
+resource "aws_db_instance" "this" {
+  identifier             = "db-${var.environment}"
+  engine                 = var.db_engine
+  instance_class         = var.db_instance_class
+  allocated_storage      = var.db_allocated_storage
 
-  # El módulo compute le añadirá reglas de ingreso (via SG). Aquí solo definimos el SG.
-  tags = {
-    Name        = "db-sg-${var.environment}"
-    Environment = var.environment
-  }
-}
+  db_name                = var.db_name
+  username               = var.db_username
+  password               = var.db_password
 
-resource "aws_db_instance" "primary" {
-  allocated_storage    = var.db_allocated_storage
-  engine               = var.db_engine
-  engine_version       = var.db_engine_version
-  instance_class       = var.db_instance_class
-  db_name              = var.db_name
-  username             = var.db_username
-  password             = var.db_password
-  db_subnet_group_name = aws_db_subnet_group.this.name
+  db_subnet_group_name   = aws_db_subnet_group.this.name
   vpc_security_group_ids = [aws_security_group.db.id]
 
-  backup_retention_period = 1
+  skip_final_snapshot    = true
+  publicly_accessible    = false
 
-  skip_final_snapshot = true
-
-  tags = {
-    Name        = "rds-primary-${var.environment}"
-    Environment = var.environment
-  }
-}
-
-resource "aws_db_instance" "replica" {
-  depends_on          = [aws_db_instance.primary]
-  replicate_source_db = aws_db_instance.primary.identifier
-  instance_class      = var.db_instance_class
-
-  skip_final_snapshot = true
-
-  tags = {
-    Name        = "rds-replica-${var.environment}"
-    Environment = var.environment
-  }
+  #esto se puede modificar dependiendoque tan seguido quiera las copias de seguridad
+  backup_retention_period = 0
 }
