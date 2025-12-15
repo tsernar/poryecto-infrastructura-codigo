c92290d9b6360c5d94b3beb562e2935b7e802195
tsernar <tsernar@unal.edu.co>
2025-12-15 03:55:21 -0500

    actualizacion en main

--- modules/db/main.tf
+++ modules/db/main.tf
@@ -30,7 +30,7 @@ resource "aws_db_instance" "primary" {
   db_subnet_group_name = aws_db_subnet_group.this.name
   vpc_security_group_ids = [aws_security_group.db.id]
 
-  backup_retention_period = 1
+  backup_retention_period = 0
 
   skip_final_snapshot = true
 
