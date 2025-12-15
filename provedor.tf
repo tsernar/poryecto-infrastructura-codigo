066b40382636ae17954f3f85c4dcea7b44fe2208
tsernar <tsernar@unal.edu.co>
2025-12-15 00:47:46 -0500

    actualizacion archivos jum

--- provedor.tf
+++ provedor.tf
@@ -4,20 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = ">= 5.0"
     }
-  }
-
-  # Backend remoto en S3 (remote state + locking en DynamoDB)
-  backend "s3" {
-    bucket         = "thomas-iac-state"    # <- cámbialo
-    key            = "multi-env/terraform.tfstate"  # puedes usar workspaces
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
   }
 }
 
 provider "aws" {
-  region = var.region
+  region = var.aws_region
 }
